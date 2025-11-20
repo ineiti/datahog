38d8fe66cc306843833964a8f637bf53b5fea761
Linus Gasser <linus.gasser@epfl.ch>
2025-11-20 14:45:24 +0100

    Trying out editors

--- frontend/src/app/view/basic/basic.ts
+++ frontend/src/app/view/basic/basic.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 // Need to go through list of https://medevel.com/notion-style-editors-21991/
 
+// Yoopta - for react
+
+// Trix -  https://github.com/basecamp/trix
+
 // TinyMCE - need to check it out. No "/", but
+import tinymce from 'tinymce';
 
 // Lexical - seems to be nice, but I cannot get it to work with slash and other
 // stuff.
@@ -38,7 +43,98 @@ export class Basic {
   async ngOnInit() {
     console.log('View-basic 2');
     this.editor = document.getElementById('editor');
-    this.lexical();
+    this.tinyMCE();
+  }
+
+  tinyMCE() {
+    tinymce.PluginManager.add('slashcommands', function (editor) {
+      var insertActions = [
+        {
+          text: 'Heading 1',
+          icon: 'h1',
+          action: function () {
+            editor.execCommand('mceInsertContent', false, '<h1>Heading 1</h1>');
+            editor.selection.select(editor.selection.getNode());
+          },
+        },
+        {
+          text: 'Heading 2',
+          icon: 'h2',
+          action: function () {
+            editor.execCommand('mceInsertContent', false, '<h2>Heading 2</h2>');
+            editor.selection.select(editor.selection.getNode());
+          },
+        },
+        {
+          text: 'Heading 3',
+          icon: 'h3',
+          action: function () {
+            editor.execCommand('mceInsertContent', false, '<h3>Heading 3</h3>');
+            editor.selection.select(editor.selection.getNode());
+          },
+        },
+        {
+          type: 'separator',
+        },
+        {
+          text: 'Bulleted list',
+          icon: 'unordered-list',
+          action: function () {
+            editor.execCommand('InsertUnorderedList', false);
+          },
+        },
+        {
+          text: 'Numbered list',
+          icon: 'ordered-list',
+          action: function () {
+            editor.execCommand('InsertOrderedList', false);
+          },
+        },
+      ];
+
+      // Register the slash commands autocompleter
+      editor.ui.registry.addAutocompleter('slashcommands', {
+        trigger: '/',
+        minChars: 0,
+        columns: 1,
+        fetch: function (pattern) {
+          const matchedActions = insertActions.filter(function (action) {
+            return (
+              action.type === 'separator' ||
+              action.text!.toLowerCase().indexOf(pattern.toLowerCase()) !== -1
+            );
+          });
+
+          return new Promise((resolve) => {
+            var results = matchedActions.map(function (action) {
+              return {
+                meta: action,
+                text: action.text,
+                icon: action.icon,
+                value: action.text,
+                type: action.type,
+              };
+            });
+            resolve(results as any);
+          });
+        },
+        onAction: function (autocompleteApi, rng, action, meta) {
+          editor.selection.setRng(rng);
+          // Some actions don't delete the "slash", so we delete all the slash
+          // command content before performing the action
+          editor.execCommand('Delete');
+          meta['action']();
+          autocompleteApi.hide();
+        },
+      });
+      return {};
+    });
+
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'slashcommands lists',
+      height: 400,
+    });
   }
 
   lexical() {
